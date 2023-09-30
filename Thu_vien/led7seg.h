02d9b1c58889e74d078c10a00309dd6c09e4cf2a
phanguyen13 <124803818+phanguyen13@users.noreply.github.com>
2023-09-30 17:09:08 +0700

    Update led7seg.h

--- Thu_vien/led7seg.h
+++ Thu_vien/led7seg.h
@@ -12,5 +12,6 @@ extern GPIO_TypeDef* ledPorts_data[];
 void print_led(int num);
 //void scanLED(int time_loop,int num);
 void set_time(int h,int m, int s);
-void scanLED(int time_loop,int h,int m, int s);
+void scanLED_time(int time_loop,int h,int m, int s);
+void scanLED_num(int time_loop,int num);
 #endif
